f57463191004074256102467ba19c69bc288aea9
Aymen Adline <51171668+aymen04@users.noreply.github.com>
2022-05-04 15:04:15 +0200

    Add files via upload

---
+++ email.rb
@@ -0,0 +1,17 @@
+class Email
+    attr_accessor :body, :subject, :to
+    attr_reader :from, :created_at, :updated_at, :sent_at
+  
+    def initialize(*args)
+      arg = args[0]
+      @body = arg[:body]
+      @subject = arg[:subject]
+      @from = arg[:from]
+      @to = arg[:to]
+      @created_at = Time.new
+      @updated_at = Time.new
+      @sent_at = nil
+    end
+  end
+  
+  p aymen = Email.new({body: "aymen", subject: "test", from: "hello"})
